5e1cfcfd68127ff2dde85e6747f2b08dda139cba
Evgeniy652 <evgen652@mail.ru>
2022-05-04 22:51:54 +0300

    feat: add start structure and style

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+		  
+	const h1 = document.createElement('h1');
+	h1.innerHTML = 'Клавиатура';
+	document.body.append(h1);
+
+
+
+
+	
+	const textarea = document.createElement('textarea');
+	textarea.classList.add('textarea');
+	document.body.append(textarea);
+	const applyTextarea = document.querySelector('.textarea');
+
+
+
+
+	const keyboard = document.createElement('div');
+	keyboard.classList.add('keyboard');
+	document.body.append(keyboard);
+	const applyKeyboard = document.querySelector('.keyboard');
+
+
+
+
+  
